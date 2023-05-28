d01795f6a1bd88dae76d4206181c641be507cd9e
Pushpak Maheshwari <PushpakMaheshwari14@gmail.com>
2023-05-28 17:41:53 +0530

    Add loginApi with JWT

--- auth_routes.py
+++ auth_routes.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter,status 
+from fastapi import APIRouter,status,Depends 
 from database import Session,engine
-from schemas import SignUpModel
+from schemas import SignUpModel,LoginModel
 from models import User
 from fastapi.exceptions import HTTPException
 from werkzeug.security import generate_password_hash,check_password_hash
+from fastapi_jwt_auth import AuthJWT
+from fastapi.encoders import jsonable_encoder
 
 auth_router = APIRouter(
     prefix="/auth",
@@ -11,6 +13,19 @@ auth_router = APIRouter(
 )
 
 session = Session(bind = engine)
+
+@auth_router.get('/')
+async def hello(Authorize:AuthJWT=Depends()):
+    try:
+        Authorize.jwt_required()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Invalid Token")
+
+    return {"hello world"}
+
+
+# SignUp Route
 
 @auth_router.post('/signup',status_code = status.HTTP_201_CREATED)
 async def signup(user:SignUpModel):
@@ -33,9 +48,42 @@ async def signup(user:SignUpModel):
         is_active = user.is_active,
         is_staff = user.is_staff
     )
-
     session.add(new_user)
-    
     session.commit()
-
     return new_user
+
+#Login Route
+
+@auth_router.post('/login',status_code=200)
+async def login(user:LoginModel,Authorize:AuthJWT=Depends()):
+    db_user = session.query(User).filter(User.username == user.username).first()
+
+    if db_user and check_password_hash(db_user.password, user.password):
+        access_token = Authorize.create_access_token(subject=db_user.username)
+        refresh_token = Authorize.create_refresh_token(subject=db_user.username)
+
+        response = {
+            "access": access_token,
+            "refresh" : refresh_token
+        } 
+
+        return jsonable_encoder(response)
+ 
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                        detail="Invalid Username or Password")
+
+# Refreshing Tokens
+
+@auth_router.get('/refresh')
+async def refresh_token(Authorized:AuthJWT=Depends()):
+    try:
+        Authorized.jwt_refresh_token_required()
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail="Please provide a valid refresh token")
+    
+    current_user =Authorized._get_jwt_subject()
+
+    access_token = Authorized.create_access_token(subject=current_user)
+
+    return jsonable_encoder({"access":access_token})
