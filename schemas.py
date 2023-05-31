141018be12533e0cd0f98daa04519ae103359713
Pushpak Maheshwari <PushpakMaheshwari14@gmail.com>
2023-05-31 22:30:20 +0530

    Add orders

--- schemas.py
+++ schemas.py
@@ -21,10 +21,25 @@ class SignUpModel(BaseModel):
             }
         }
 
-
 class Settings(BaseModel):
     authjwt_secret_key:str='d2629c262616373df381db932322d5555c71ae6dfece36644da3ddf0cdb20913'
 
 class LoginModel(BaseModel):
     username:str
     password:str
+
+class OrderModel(BaseModel):
+    id : Optional[int]
+    quantity : int
+    order_status : Optional[str] = "PENDING" 
+    pizza_size : Optional[str] = "SMALL"
+    user_id : Optional[int]
+
+    class Config:
+        orm_mode = True
+        schema_extra = {
+            "example":{
+                "quantity" : 2,
+                "pizza_size":"LARGE" 
+            }
+        }
