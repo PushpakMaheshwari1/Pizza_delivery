76c7788723e68612331fc1e44526fabf213f9fa5
Pushpak Maheshwari <PushpakMaheshwari14@gmail.com>
2023-06-15 17:35:07 +0530

    import CORSMiddleware to enable sharing resources between different website

--- main.py
+++ main.py
@@ -3,8 +3,22 @@ from auth_routes import auth_router
 from order_routes import order_router
 from fastapi_jwt_auth import AuthJWT
 from schemas import Settings
+# from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+# origins = [
+#    "http://127.0.0.1:5500"  # Replace with the actual URL of your web page
+# ]
+
+# app.add_middleware(
+#    CORSMiddleware,
+#    allow_origins=origins,
+#    allow_credentials=True,
+#    allow_methods=["*"],
+#    allow_headers=["*"],
+# )
 
 @AuthJWT.load_config
 def get_config(): 
